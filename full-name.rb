d9b11550c4042bb2286860a03b51440ee5426bcd
Balint Nemes <squarebe03@gmail.com>
2015-07-03 10:45:59 +0100

    full-name

---
+++ full-name.rb
@@ -0,0 +1,7 @@
+puts "What is your firstname?"
+firstname = gets.chomp
+puts "And your surname?"
+surname = gets.chomp
+puts "Doyou have any middle names?"
+midname = gets.chomp
+puts "In this case, have a nice day " + firstname + midname + surname + ", it is nice to meet you!"

c7fb8e68d95dd006543d1379e8956297461ca0cf
Balint Nemes <squarebe03@gmail.com>
2015-07-03 10:48:35 +0100

    fixed typo in fewthingstotry1

---
+++ few-things-to-try.rb
@@ -0,0 +1,16 @@
+# how many hours in a year?:
+puts
+puts (365*24)
+puts
+# how many minutes are in a decade?:
+puts
+puts ((10*365*24)+2.5)*60
+puts
+# how many seconds old are you?:
+# i was 39 on 23rd of march this year. 3 months and 11 days ago today
+puts
+puts ((39*365)+103)*24*60*60
+puts
+# how many chockolates do you hope to eat in your life?:
+puts
+puts "since i have no planed end-date its irrevelant, but to be honest a lot!"

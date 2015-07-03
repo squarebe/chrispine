6b1003d00959fbb9644a7e3fc7fd0c04250eca2f
Balint Nemes <squarebe03@gmail.com>
2015-07-03 12:17:24 +0100

    fav-#-fixed

--- favorite-number.rb
+++ favorite-number.rb
@@ -1,4 +1,4 @@
 puts 'What is your favorite number?'
 numba = gets.chomp.to_i
-mine = numba + 1
-puts 'you see my favorite is bigger: ' + mine.to_i
+mine = numba.to_i + 1
+puts 'you see my favorite is bigger: ' + mine.to_s

14c8d9480f41766d6f51f8c989e943036d9ea584
Balint Nemes <squarebe03@gmail.com>
2015-07-06 12:24:45 +0100

    spanker

---
+++ birthday-spanker.rb
@@ -0,0 +1,16 @@
+puts 'Which year you were born?'
+year = gets.chomp.to_s
+puts 'Which month you were born?'
+month = gets.chomp.to_s
+puts 'On what day you were born?'
+day = gets.chomp.to_s
+# puts 'Your day of born then: ' + day + ', ' + month + ', ' + year + '.'
+def calculate_number_of_years(year, month, day)
+	minutes = Time.new - Time.mktime(year, month, day)
+	years = ((minutes/525949)/60).to_i
+end
+years = calculate_number_of_years(year, month, day)
+
+puts 'Now you are gonna get just enough spanking!'
+puts
+puts 'SPANK!' * years
